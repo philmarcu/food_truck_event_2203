90c3a94603424d57459589c3b2a9da48dd2f931e
philmarcu <philmark18@gmail.com>
2022-04-26 09:27:55 -0600

    Finish Iteration 1

--- spec/food_truck_spec.rb
+++ spec/food_truck_spec.rb
@@ -4,10 +4,22 @@ require './lib/food_truck'
 RSpec.describe FoodTruck do
   before :each do
     @food_truck = FoodTruck.new("Rocky Mountain Pies")
+    @item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+    @item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
   end
 
   it 'exists & has attributes' do
     expect(@food_truck.name).to eq("Rocky Mountain Pies")
     expect(@food_truck.inventory).to eq({})
+  end
+
+  it 'can check stock' do
+    expect(@food_truck.check_stock(@item1)).to be(0)
+  end
+
+  it 'can add stock' do
+    @food_truck.stock(@item1, 30)
+    @food_truck.stock(@item1, 25)
+    expect(@food_truck.check_stock(@item1)).to eq(55)
   end
 end
